1a431453602dfd459308e420d0ec306ade312c29
MathTeacherSmith <Clarence.Smith@vertexacademies.org>
2024-10-15 14:02:32 -0400

    Add files via upload

---
+++ Clarence_Smith_JS_01.js
@@ -0,0 +1,13 @@
+// This is a comment.  The // tells the computer to ignore this line.
+// Developers use comments to leave helpful notes to themselves and others.
+
+// Add your code below and click the 'Run code' button to see if it works!
+// Problem #2
+console.log("¡Hola, Mundo!")
+
+// Problem #3
+console.log("Hello,");
+console.log("World!");
+
+// Problem #4
+console.log("The computer says, \"Hello World!\"");
